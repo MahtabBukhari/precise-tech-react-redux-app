8771a24a9d19ef25c7782ecfcaf620c1a9628fa6
MahtabBukhari <git config --global user.email you@example.com>
2022-10-17 16:36:35 +0500

    setup react app plus redux structure

---
+++ src/Redux/store.js
@@ -0,0 +1,12 @@
+import {createStore,applyMiddleware,combineReducers} from 'redux';
+import {composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+
+
+const rootReducer = combineReducers({
+
+})
+
+const initialState={}
+
+export const store = createStore(rootReducer,initialState,composeWithDevTools(applyMiddleware(thunk)))
